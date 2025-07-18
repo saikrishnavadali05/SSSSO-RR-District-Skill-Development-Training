7302a735948f13925b81487b13274447da02af1a
Navya463 <anthatinavyasri564@gmail.com>
2025-07-18 07:30:47 +0530

    Mini project.py

--- may_2025_contributions/PY-F-140_Anthati Navyasri_submission/Mini project.py
+++ may_2025_contributions/PY-F-140_Anthati Navyasri_submission/Mini project.py
@@ -1,15 +1,29 @@
 GLOBAL TIME IN CITIES:
+Code:
 
- from datetime import datetime
 import pytz
-def get_time_in_timezone(timezone):
-    tz = pytz.timezone(timezone)
-    current_time = datetime.now(tz)
-    return current_time
-# Example usage
-print(get_time_in_timezone('US/Pacific'))
-print(get_time_in_timezone('Asia/Kolkata'))
+from datetime import datetime
 
-EXPECTED OUTPUT:
-2025-07-17 01:58:12.079049-07:00
-2025-07-17 14:28:12.080409+05:30
+def get_city_time(city, timezone):
+    city_time = datetime.now(pytz.timezone(timezone))
+    return city_time.strftime(f"%Y-%m-%d %H:%M:%S {city}")
+
+cities = {
+    "New York": "America/New_York",
+    "London": "Europe/London",
+    "Tokyo": "Asia/Tokyo",
+    "Sydney": "Australia/Sydney",
+    "Mumbai": "Asia/Kolkata"
+}
+
+for city, timezone in cities.items():
+    print(get_city_time(city, timezone))
+
+
+ Expected Output:
+
+2025-07-17 07:28:00 New York
+2025-07-17 12:28:00 London
+2025-07-18 00:28:00 Tokyo
+2025-07-18 02:28:00 Sydney
+2025-07-17 17:58:00 Mumbai
